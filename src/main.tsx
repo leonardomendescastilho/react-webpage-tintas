e30e7bef37495ee37921c273801ff0d572ab5760
Leonardo Mendes <leonardo.smendescastilho@gmail.com>
2025-12-29 18:42:00 -0300

    feat(ui): adiciona suporte a dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
+import { ThemeProvider } from '@/components/theme-provider'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <ThemeProvider defaultTheme="system" storageKey="prana-ui-theme">
+        <App />
+      </ThemeProvider>
     </HelmetProvider>
   </StrictMode>,
 )
